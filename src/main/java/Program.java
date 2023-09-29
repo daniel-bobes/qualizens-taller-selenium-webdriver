30d9dce0f546f47bb01dbb4b49e643babf2a9b4d
Daniel Bobes de Pedro <danibobesdepedro@gmail.com>
2023-09-29 19:10:03 +0200

    Buenas prácticas - Utilizando la interfaz WebDriver como tipo para el driver

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Program {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Program {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+
+        WebDriver driver =  new FirefoxDriver();
+
+        driver.quit();
+
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+
+        driver =  new ChromeDriver();
+
+        driver.quit();
+    }
 }
